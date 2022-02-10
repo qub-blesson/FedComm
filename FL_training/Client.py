b41d4d4c74cc6bc8223abb8acf1a26da2f7f8776
Gary Cleland <gcleland99@outlook.com>
2022-02-10 21:31:20 +0000

    Fix NameError

--- FL_training/Client.py
+++ FL_training/Client.py
@@ -83,4 +83,4 @@ class Client(Communicator):
     def finish(self):
         logger.info(self.packets_sent)
         self.tcp_sock.connect((self.server_addr, self.server_port))
-        self.send_msg_tcp_client(self.tcp_sock, msg)
+        self.send_msg_tcp_client(self.tcp_sock, self.computation_time)
