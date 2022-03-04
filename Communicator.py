cdb3d87fb6c931e1d8c2443e1f88923479429d9d
Gary Cleland <gcleland99@outlook.com>
2022-03-04 15:41:35 +0000

    Add Test resource

--- Communicator.py
+++ Communicator.py
@@ -16,6 +16,7 @@ import config
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+
 class Communicator(object):
     def __init__(self, index, ip_address, host, port, pub_topic='fedadapt', sub_topic='fedadapt', client_num=0):
         self.client_id = index
@@ -42,6 +43,18 @@ class Communicator(object):
         response = self.client.get('test')
         print(response)
 
+
 class CoAPServer(CoAP):
     def __init__(self, host, port):
         CoAP.__init__(self, (host, port))
+        self.add_resource('test/', Test())
+
+
+class Test(Resource):
+    def __init__(self, name="Sensor", coap_server=None):
+        super(Test, self).__init__(name, coap_server, visible=True, observable=True, allow_children=True)
+        self.payload = "This is a new test"
+        self.content_type = "text/plain"
+
+    def render_GET(self, request):
+        return "test returned"
