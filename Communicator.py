27badcf8b62b2048af835e43082df9739485d9cd
Gary Cleland <gcleland99@outlook.com>
2022-03-04 01:04:51 +0000

    Add coap server

--- Communicator.py
+++ Communicator.py
@@ -7,6 +7,8 @@ import logging
 from coapthon.client.helperclient import HelperClient
 from coapthon.messages.request import Request
 from coapthon import defines
+from coapthon.resources.resource import Resource
+from coapthon.server.coap import CoAP
 
 import config
 
@@ -21,12 +23,24 @@ class Communicator(object):
         self.payload = 'text/plain'
         self.client = HelperClient(server=(host, port))
 
-        request = Request()
-        request.code = defines.Codes.GET.number
-        request.type = defines.Types['NON']
-        request.destination = (host, port)
-        request.uri_path = self.path
-        # request.content_type = defines.Content_types["application/xml"]
-        request.payload = 'GIVE DATA'
-        response = self.client.send_request(request)
-        print(response)
+        if index != config.K:
+            request = Request()
+            request.code = defines.Codes.GET.number
+            request.type = defines.Types['NON']
+            request.destination = (host, port)
+            request.uri_path = self.path
+            # request.content_type = defines.Content_types["application/xml"]
+            request.payload = 'GIVE DATA'
+            response = self.client.send_request(request)
+            print(response)
+        else:
+            server = CoAPServer(host, port)
+            try:
+                server.listen(100)
+            except KeyboardInterrupt:
+                print("Server shutting down")
+                server.close()
+
+class CoAPServer(CoAP):
+    def __init__(self, host, port):
+        CoAP.__init__(self, (host, port))
