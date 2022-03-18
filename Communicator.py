3234ab2399a9d00aee808466dc6b12b44c5da89a
Gary Cleland <gcleland99@outlook.com>
2022-03-18 14:30:06 +0000

    Create functions to connect sockets

--- Communicator.py
+++ Communicator.py
@@ -12,25 +12,45 @@ import config
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+
 class Communicator(object):
     def __init__(self, index, ip_address, host, port, pub_topic='fedadapt', sub_topic='fedadapt', client_num=0):
         self.q = Queue()
+        self.index = index
+        self.host = host
+        self.port = port
         # set 0MQ context
         self.context = zmq.Context()
-        # open publish sockets for all devices
-        self.pub_socket = self.context.socket(zmq.PUB)
-        self.pub_socket.bind("tcp://*:%s" % port)
-        self.sub_socket = self.context.socket(zmq.SUB)
-        time.sleep(15)
+        # init pub socket
+        self.pub_socket = None
+        self.sub_socket = None
+        self.client_to_server()
+        self.server_to_client()
 
         # server subscribes to all clients
-        #if index == config.K:
-        #    for i in config.CLIENTS_LIST:
-        #        self.sub_socket.connect("tcp://"+i+":"+str(port))
-        #else:
-            # subscribe to server from clients
-        self.sub_socket.connect("tcp://" + host + ":" + str(port))
-        self.sub_socket.subscribe(b'')
+        # if index == config.K:
+        #
+        # else:
+        # subscribe to server from clients
+
+    def client_to_server(self):
+        if self.index == config.K:
+            self.pub_socket = self.context.socket(zmq.PUB)
+            self.pub_socket.bind("tcp://*:%s" % self.port)
+        else:
+            time.sleep(5)
+            self.sub_socket = self.context.socket(zmq.SUB)
+            self.sub_socket.connect("tcp://" + self.host + ":" + str(self.port))
+            self.sub_socket.subscribe(b'')
+
+    def server_to_client(self):
+        if self.index == config.K:
+            time.sleep(10)
+            for i in config.CLIENTS_LIST:
+                self.sub_socket.connect("tcp://"+i+":"+str(self.port))
+        else:
+            self.pub_socket = self.context.socket(zmq.PUB)
+            self.pub_socket.bind("tcp://*:%s" % self.port)
 
     def send_msg(self, msg):
         msg_pickle = pickle.dumps(msg)
