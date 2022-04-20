465c600c8b6b58f376205889721a6d03826173ab
Gary Cleland <gcleland99@outlook.com>
2022-04-20 16:47:16 +0100

    Add integration test

--- TestSuite/IntegrationTest.py
+++ TestSuite/IntegrationTest.py
@@ -8,6 +8,5 @@ import FL.Run as Run
 
 class IntegrationTest(unittest.TestCase):
     def test_system(self):
-        """os.system('python3 ../FL.Run')"""
-
-
+        os.system('python3 ../FL.Run --communicator TCP --model VGG5 --limiter Wi-Fi --rounds 1')
+        self.assertTrue(False)
