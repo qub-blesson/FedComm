8b8c3d24269f55ed0746e89a9b670d5e2eade72b
Gary Cleland <gcleland99@outlook.com>
2022-05-06 20:27:03 +0100

    Add text for input args

--- FL/Run.py
+++ FL/Run.py
@@ -20,12 +20,12 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--communicator', help='Select communication protocol: TCP, UDP, ZMTP, AMQP, MQTT',
                         default='TCP')
-    parser.add_argument('--model', help='Model type', default='VGG8')
-    parser.add_argument('--stress', help='Tool used to limit network or apply stress: CPU, NET, ALL', default=None)
-    parser.add_argument('--limiter', help='Tool used to limit network or apply stress: 3G, 4G, Wi-Fi', default=None)
+    parser.add_argument('--model', help='Model type: VGG5 or VGG8', default='VGG8')
+    parser.add_argument('--stress', help='Tool used to apply stress: CPU, NET, ALL', default=None)
+    parser.add_argument('--limiter', help='Tool used to limit network: 3G, 4G, Wi-Fi', default=None)
     parser.add_argument('--rounds', help='Number of training rounds', type=int, default=5)
     parser.add_argument('--monitor', help='Monitors packet loss rate: True', default=None)
-    parser.add_argument('--target', help='Server', default='127.0.0.1')
+    parser.add_argument('--target', help='Server IP', default='127.0.0.1')
     args = parser.parse_args()
     # set parameters based on input
     stress = args.stress
